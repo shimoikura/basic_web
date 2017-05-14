035b2c5dfa0d00848bb5f040b88ba660a4231b93
atm0016 <atm0016@example.com>
2017-05-15 00:04:52 +0530

    calculater's main function complete

--- calculater/js/calculater.js
+++ calculater/js/calculater.js
@@ -1,23 +1,76 @@
 $(document).ready(function(){
-  var i = 0;
-  var num1Array = new Array();  //配列の作成
-  var num2Array = new Array();
+  var i = 0;   //THE FIRST NUMBER配列の作成用
+  var k = 0;   //THE SECOND NUMBER配列の作成用
+  var num1Array = new Array();  //THE FIRST NUMBER用配列の作成
+  var num2Array = new Array(); //THE FIRST NUMBER用配列の作成
+  var num3Array = new Array(); //THE SECOND NUMBER用配列の作成
+  var num4Array = new Array(); //THE SECOND NUMBER用配列の作成
+  var num1;
+  var num2;
+  var cal_id;
   var cal_class;
   num2Array[0] = [];
+  num4Array[0] = [];
   $(".calculater-bottom td").on('click',function(){
      cal_class = $(this).attr("class");　// classの取得
-    if (cal_class == "number")
-    {
-      
-      $("#a").text(i);
+    if ((cal_class == "number" ) && ($.isEmptyObject(cal_id) == true)){
       num1Array[i+2] = $(this).attr("id");  //idの値取得
       num2Array[i+1] = num2Array[i] + num1Array[i+2];
-      $("#first-num").text(num2Array[i+1]);
+      num1 = num2Array[i+1];
+      $("#first-num").text(num1);
       i++;
     }
-    else {
-      cal_class = $(this).attr("id");　// classの取得
-      $("#arithmatic_text").text(cal_class);
+    else if ((cal_class == "arithmatic") && ($.isEmptyObject(num1) == false)){
+      cal_id = $(this).attr("id");　// classの取得
+      $("#arithmatic_text").text(cal_id);
     }
+    else if ((cal_class == "number") && ($.isEmptyObject(cal_id) == false)) {
+      num3Array[k+2] = $(this).attr("id");  //idの値取得
+      num4Array[k+1] = num4Array[k] + num3Array[k+2];
+      num2 = num4Array[k+1]
+      $("#second-num").text(num2);
+      k++;
+    }
+    else if ((cal_class == "arithmatic") && ($.isEmptyObject(num1) == true)) {
+      $("#first-num").text("★Please enter !");
+    }
+    else if (cal_class == "result") {
+      num2Array[0] = [];
+      num4Array[0] = [];
+      num1 = "";
+      num2 = "";
+      i = 0;
+      k = 0;
+      cal_id = "";
+      $("#first-num").text("");
+      $("#arithmatic_text").text("");
+      $("#second-num").text("");
+      $("#enter_text").text("");
+    }
+    else {
+      $("#first-num").text("禁止");
+      $("#arithmatic_text").text("禁止");
+      $("#second-num").text("禁止");
+    }
+
+    //HERE button click
+    $("#enter_button").click(function(){
+      if (cal_id == "+") {
+        var result = parseInt(num1) + parseInt(num2);
+        $("#enter_text").text(result);
+      }
+      else if (cal_id == "-") {
+        var result = num1 - num2;
+        $("#enter_text").text(result);
+      }
+      else if (cal_id == "×") {
+        var result = num1 * num2;
+        $("#enter_text").text(result);
+      }
+      else if (cal_id == "÷") {
+        var result = num1 / num2;
+        $("#enter_text").text(result);
+      }
+    });
   });
 });
